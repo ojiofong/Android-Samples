e25478f7b6c01e2038c5f40ca99af808b3a7667c
Oji Ofong <ojiofong@gmail.com>
2017-04-30 00:10:42 -0400

    Added ICallBack interface to Fragment

---
+++ app/src/main/java/com/ojiofong/androidsamples/fragment/FragmentSampleHelper.java
@@ -0,0 +1,70 @@
+package com.ojiofong.androidsamples.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ojiofong.androidsamples.R;
+
+/**
+ * Created by ojiofong on 4/30/17.
+ * Just contains Fragments used by FragmentSampleActivity
+ */
+
+public class FragmentSampleHelper {
+
+    /**
+     * Fragment class
+     */
+    public static class MFragment extends Fragment {
+        public static String TAG = MFragment.class.getSimpleName();
+        private ICallBack callBack;
+
+        public interface ICallBack {
+            void doSomething();
+        }
+
+        public MFragment() {
+        }
+
+        @Override
+        public void onAttach(Context context) {
+            super.onAttach(context);
+            callBack = (ICallBack) getActivity();
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.activity_sensor, container, false);
+            rootView.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.colorAccent));
+            rootView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    callBack.doSomething();
+                }
+            });
+            return rootView;
+        }
+    }
+
+    public static class MDialogFragment extends DialogFragment {
+        public static String TAG = MDialogFragment.class.getSimpleName();
+
+        public MDialogFragment() {
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.activity_sensor, container, false);
+            return rootView;
+        }
+    }
+}
--- app/src/main/java/com/ojiofong/androidsamples/ui/FragmentSampleActivity.java
+++ app/src/main/java/com/ojiofong/androidsamples/ui/FragmentSampleActivity.java
@@ -2,27 +2,17 @@ package com.ojiofong.androidsamples.ui;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.DialogFragment;
-import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ojiofong.androidsamples.R;
-
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import com.ojiofong.androidsamples.fragment.FragmentSampleHelper;
 
 /**
  *
  */
-public class FragmentSampleActivity extends AppCompatActivity {
+public class FragmentSampleActivity extends AppCompatActivity implements FragmentSampleHelper.MFragment.ICallBack {
 
     private static final String TAG = FragmentSampleActivity.class.getSimpleName();
 
@@ -41,44 +31,17 @@ public class FragmentSampleActivity extends AppCompatActivity {
     }
 
     private void setupFragment() {
-        MFragment frag = new MFragment();
-        getSupportFragmentManager().beginTransaction().replace(R.id.container, frag, MFragment.TAG).commit();
+        FragmentSampleHelper.MFragment frag = new FragmentSampleHelper.MFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, frag, FragmentSampleHelper.MFragment.TAG).commit();
     }
 
     private void setupDialogFragment() {
-        MDialogFragment dialogFrag = new MDialogFragment();
-        dialogFrag.show(getSupportFragmentManager(), MDialogFragment.TAG);
+        FragmentSampleHelper.MDialogFragment dialogFrag = new FragmentSampleHelper.MDialogFragment();
+        dialogFrag.show(getSupportFragmentManager(), FragmentSampleHelper.MDialogFragment.TAG);
     }
 
-    /**
-     * Fragment class
-     */
-    public static class MFragment extends Fragment {
-        public static String TAG = MFragment.class.getSimpleName();
-
-        public MFragment() {
-        }
-
-        @Nullable
-        @Override
-        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.activity_sensor, container, false);
-            rootView.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.colorAccent));
-            return rootView;
-        }
-    }
-
-    public static class MDialogFragment extends DialogFragment {
-        public static String TAG = MDialogFragment.class.getSimpleName();
-
-        public MDialogFragment() {
-        }
-
-        @Nullable
-        @Override
-        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.activity_sensor, container, false);
-            return rootView;
-        }
+    @Override
+    public void doSomething() {
+        Toast.makeText(this, "doSomething", Toast.LENGTH_SHORT).show();
     }
 }

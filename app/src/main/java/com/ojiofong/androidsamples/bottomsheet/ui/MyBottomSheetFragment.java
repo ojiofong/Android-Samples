a5873f06cf8713cc975005a3e2b7c08be791570a
Oji Ofong <ojiofong@gmail.com>
2018-03-09 08:37:54 -0500

    Dynamically change BottomSheet layout on newState

--- app/src/main/java/com/ojiofong/androidsamples/bottomsheet/ui/MyBottomSheetFragment.java
+++ app/src/main/java/com/ojiofong/androidsamples/bottomsheet/ui/MyBottomSheetFragment.java
@@ -18,6 +18,7 @@ import com.ojiofong.androidsamples.R;
 
 public class MyBottomSheetFragment extends BottomSheetDialogFragment {
     private static String TAG = MyBottomSheetFragment.class.getSimpleName();
+    private BottomSheetBehavior behavior;
 
     private BottomSheetBehavior.BottomSheetCallback bottomSheetCallback = new BottomSheetBehavior.BottomSheetCallback() {
         @Override
@@ -26,6 +27,13 @@ public class MyBottomSheetFragment extends BottomSheetDialogFragment {
             if (newState == BottomSheetBehavior.STATE_HIDDEN) {
                 dismiss();
             }
+
+            if (newState == BottomSheetBehavior.STATE_EXPANDED) {
+                setLayout(R.layout.fragment_bottomsheet_expanded, newState);
+            } else if (newState == BottomSheetBehavior.STATE_COLLAPSED) {
+                setLayout(R.layout.fragment_bottomsheet, newState);
+            }
+
         }
 
         @Override
@@ -46,11 +54,23 @@ public class MyBottomSheetFragment extends BottomSheetDialogFragment {
         super.setupDialog(dialog, style);
         View rootView = View.inflate(getContext(), R.layout.fragment_bottomsheet, null);
         dialog.setContentView(rootView);
-        BottomSheetBehavior behavior = BottomSheetBehavior.from((View) rootView.getParent());
+        behavior = BottomSheetBehavior.from((View) rootView.getParent());
         if (behavior != null) {
             int heightInPixels = getActivity().getResources().getDimensionPixelSize(R.dimen.bottom_sheet_peek_height);
             behavior.setPeekHeight(heightInPixels);
             behavior.setBottomSheetCallback(bottomSheetCallback);
+        }
+    }
+
+    private void setLayout(int layoutResource, int state) {
+        View rootView = View.inflate(getContext(), layoutResource, null);
+        getDialog().setContentView(rootView);
+        behavior = BottomSheetBehavior.from((View) rootView.getParent());
+        if (behavior != null) {
+            int heightInPixels = getActivity().getResources().getDimensionPixelSize(R.dimen.bottom_sheet_peek_height);
+            behavior.setPeekHeight(heightInPixels);
+            behavior.setBottomSheetCallback(bottomSheetCallback);
+            behavior.setState(state);
         }
     }
 }
